4656001867df08445804ddc1916f90e5fbc9bc26
SUDARAT THONGTO <sthontoo@gmail.com>
2021-04-27 15:38:57 +0700

    Storybook deployment with GitHub action

--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -2,6 +2,17 @@ import React from "react";
 
 import { Avatar } from "./Avatar";
 
+// New story using controls
+const Template = args => <Avatar {...args} />;
+
+export const Controls = Template.bind({});
+  Controls.args = {
+    loading: false,
+    size: 'tiny',
+    username: 'Dominic Nguyen',
+    src: 'https://avatars2.githubusercontent.com/u/263385',
+};
+
 export default {
   title: "Design System/Avatar",
   component: Avatar,
